29e9e88426c15504314e650fa3b2d6224040e35a
Eqnoxx <81754864+Eqno@users.noreply.github.com>
2022-12-20 23:28:46 +0800

    Add sign up and log in with json data
    Json files can be found in directory /server/data/user

--- server/website/backend.py
+++ server/website/backend.py
@@ -1,68 +1,93 @@
 # -*- coding: utf-8 -*-
 
-import flask
+import flask, json, os
 from .utils import generate_return_data, StatusCode
 
-user_database = {
-    # admin, administrator
-    'admin': '200ceb26807d6bf99fd6f4f0d1ca54d4'
-}
-
+data_base_path = os.getcwd().replace('\\', '/')+'/data/'
+msg_data_path = data_base_path + './msg/'
+user_data_path = data_base_path + './user/'
 
 def session_get_username():
     return flask.session.get('username', None)
 
-
 def session_set_username(username: str):
     flask.session['username'] = username
-
 
 def session_del_username():
     return flask.session.pop('username')
 
-
 def api_account_username():
+    ret_code, ret_msg = StatusCode.ERR_ACCOUNT_NOT_LOGINED, None
     username = session_get_username()
-    if username:
-        return generate_return_data(StatusCode.SUCCESS, {'username': username})
-    return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
 
+    if username: ret_code, ret_msg = StatusCode.SUCCESS, {'username': username}
+    return generate_return_data(ret_code, ret_msg)
+
+def find_or_create_data_dir(dir:str):
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 def api_account_login():
+    ret_code = StatusCode.ERR_ACCOUNT_USERNAME_OR_PASSWORD_WRONG
+    find_or_create_data_dir(user_data_path)
+
     data = flask.request.get_json()
     username = data['username']
     password = data['password']
-    if username in user_database:
-        if password == user_database[username]:
-            session_set_username(username)
-            return generate_return_data(StatusCode.SUCCESS)
-    return generate_return_data(
-        StatusCode.ERR_ACCOUNT_USERNAME_OR_PASSWORD_WRONG)
+
+    try:
+        with open(user_data_path + username + '.json', 'r') as f:
+            user_data = json.load(f)
+
+            if username == user_data['username'] and password == user_data['password']:
+                session_set_username(username)
+                ret_code = StatusCode.SUCCESS
+            
+            f.close()
+    except: pass
+    return generate_return_data(ret_code)
 
 def api_account_signup():
-    data = flask.request.get_json()
-    username = data['username']
-    password = data['password']
-    if username in user_database:
-        return generate_return_data(StatusCode.ERR_ACCOUNT_USERNAME_EXISTED)
-    user_database[username] = password
-    session_set_username(username)
-    return generate_return_data(StatusCode.SUCCESS)
+    ret_code = StatusCode.ERR_ACCOUNT_USERNAME_EXISTED
+    find_or_create_data_dir(user_data_path)
+
+    http_data = flask.request.get_json()
+    username = http_data['username']
+    password = http_data['password']
+
+    try:
+        with open(user_data_path + username + '.json', 'r') as f:
+            f.close()
+    except:
+        with open(user_data_path + username + '.json', 'w') as f:
+            user_data = {}
+
+            user_data['username'] = username
+            user_data['password'] = password
+            
+            ret_code = StatusCode.SUCCESS
+            session_set_username(username)
+
+            json.dump(user_data, fp=f)
+            f.close()
+
+    return generate_return_data(ret_code)
 
 def api_account_logout():
+    ret_code = StatusCode.ERR_ACCOUNT_NOT_LOGINED
     username = session_get_username()
-    if username:
+
+    if username is not None:
         if session_del_username():
-            return generate_return_data(StatusCode.SUCCESS)
-        else:
-            return generate_return_data(StatusCode.ERR_SERVER_UNKNOWN)
-    return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
+            ret_code = StatusCode.SUCCESS
+        else: ret_code = StatusCode.ERR_SERVER_UNKNOWN
+    return generate_return_data(ret_code)
 
 
 def api_game_core_image():
     result = {'url': '/static/capoo.png'}
     return generate_return_data(0, result)
-
 
 backend_pages = {
     '/api/account/username': api_account_username,
