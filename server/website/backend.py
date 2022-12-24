92002d35bd1216240f7ba952d79b9d417e37f943
Eqnoxx <81754864+Eqno@users.noreply.github.com>
2022-12-24 14:05:25 +0800

    Add avatar path
    Modify user info file path, add a root dir

--- server/website/backend.py
+++ server/website/backend.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
 
-import os, json, flask, pathlib, sys, time, threading
+############################### IMPORT ###############################
+
+import os, json, flask, pathlib, sys, time, simple_websocket
 from .utils import generate_return_data, StatusCode
-import simple_websocket
 
 sys.path.append(os.getcwd().replace('\\', '/') + '/../')
 from logic import *
+
+############################### PARAMS ###############################
 
 SOCKET_ONLINE_TIME_INTERVAL = 0.1
 CHECL_ONLINE_TIME_INTERVAL = 5
 
 data_base_path = pathlib.Path(os.getcwd()) / 'data'
+
 msg_data_path = data_base_path / 'msg'
 user_data_path = data_base_path / 'user'
-filename_suffix = '.json'
 
+info_file_name = 'info.json'
+avatar_file_name = 'avatar.png'
+
+############################## INITIAL ###############################
 
 def backend_init():
+
     for _dir in (data_base_path, msg_data_path, user_data_path):
         if not _dir.exists():
             os.mkdir(_dir)
 
+############################# GET SESSION #############################
 
 def session_get_username():
     return flask.session.get('username', None)
@@ -33,6 +42,7 @@ def session_set_username(username: str):
 def session_del_username():
     return flask.session.pop('username')
 
+############################ GET USER DATA ############################
 
 def api_account_username():
     username = session_get_username()
@@ -41,42 +51,81 @@ def api_account_username():
         return generate_return_data(StatusCode.SUCCESS, {'username': username})
     return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
 
+def api_account_userinfo():
+
+    data = flask.request.get_json()
+    username = data['username']
+
+    user_file_path = user_data_path / username / info_file_name
+
+    if not user_file_path.exists():
+        return generate_return_data(
+            StatusCode.ERR_ACCOUNT_USERNAME_NOT_EXISTED)
+
+    with open(user_file_path, 'r') as f:
+        
+        raw_user_info = json.load(f)
+        userinfo = {}
+        userinfo['username'] = raw_user_info['username']
+        userinfo['avatar'] = '/static/avatar.png'
+
+        return generate_return_data(StatusCode.SUCCESS, {'userinfo': userinfo})
+
+
+
+
+
+
+
+
 
 def api_account_login():
+
     data = flask.request.get_json()
     username = data['username']
     password = data['password']
 
-    user_file_path = user_data_path / (username + filename_suffix)
+    user_root_path = user_data_path / username
+    user_info_path = user_root_path / info_file_name
+    user_avatar_path = user_root_path / avatar_file_name
 
-    if user_file_path.exists():
-        with open(user_file_path, 'r') as f:
+    if user_info_path.exists():
+
+        with open(user_info_path, 'r') as f:
+
             user_data = json.load(f)
-            if username == user_data['username'] and password == user_data[
-                    'password']:
+            if username == user_data['username'] and password == user_data['password']:
                 session_set_username(username)
-                return generate_return_data(StatusCode.SUCCESS)
+
+        if user_avatar_path.exists():
+            flask.send_file(user_avatar_path, mimetype='image/png')
+
+    return generate_return_data(StatusCode.SUCCESS)
 
     return generate_return_data(
         StatusCode.ERR_ACCOUNT_USERNAME_OR_PASSWORD_WRONG)
 
 
 def api_account_signup():
+
     data = flask.request.get_json()
     username = data['username']
     password = data['password']
 
-    user_file_path = user_data_path / (username + filename_suffix)
+    user_root_path = user_data_path / username
+    user_info_path = user_root_path / info_file_name
+    user_avatar_path = user_root_path / avatar_file_name
 
-    if user_file_path.exists():
+    if user_root_path.exists():
         return generate_return_data(StatusCode.ERR_ACCOUNT_USERNAME_EXISTED)
 
-    with open(user_file_path, 'w') as f:
+    with open(user_info_path, 'w') as f:
         user_info = {}
 
         user_info['username'] = username
         user_info['password'] = password
 
+        user_info['avatar'] = user_avatar_path
         user_info['state'] = 'offline'
 
         user_info['friends'] = []
@@ -99,35 +148,19 @@ def api_account_logout():
     return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
 
 
-def api_account_userinfo():
-    data = flask.request.get_json()
-    username = data['username']
-
-    user_file_path = user_data_path / (username + filename_suffix)
-
-    if not user_file_path.exists():
-        return generate_return_data(
-            StatusCode.ERR_ACCOUNT_USERNAME_NOT_EXISTED)
-
-    with open(user_file_path, 'r') as f:
-        raw_user_info = json.load(f)
-        userinfo = {}
-        userinfo['username'] = raw_user_info['username']
-        userinfo['avatar'] = '/static/avatar.png'
-        return generate_return_data(StatusCode.SUCCESS, {'userinfo': userinfo})
 
 
 def api_account_add_friend():
     data = flask.request.get_json()
 
     current_username = session_get_username()
-    current_filepath = user_data_path / (current_username + filename_suffix)
+    current_filepath = user_data_path / current_username / info_file_name
 
     if not current_filepath.exists():
         return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
 
     target_username = data['username']
-    target_filepath = user_data_path / (target_username + filename_suffix)
+    target_filepath = user_data_path / target_username / info_file_name
 
     if not target_filepath.exists():
         return generate_return_data(
@@ -193,7 +226,7 @@ def api_account_get_friends():
     if not username:
         return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
 
-    user_file_path = user_data_path / (username + filename_suffix)
+    user_file_path = user_data_path / username / info_file_name
     if not user_file_path.exists():
         return generate_return_data(StatusCode.ERR_SERVER_UNKNOWN)
 
@@ -218,13 +251,13 @@ def api_account_approved_application():
     data = flask.request.get_json()
 
     current_username = session_get_username()
-    current_filepath = user_data_path / (current_username + filename_suffix)
+    current_filepath = user_data_path / current_username / info_file_name
 
     if not current_filepath.exists():
         return generate_return_data(StatusCode.ERR_ACCOUNT_NOT_LOGINED)
 
     target_username = data['username']
-    target_filepath = user_data_path / (target_username + filename_suffix)
+    target_filepath = user_data_path / target_username / info_file_name
 
     if not target_filepath.exists():
         return generate_return_data(
@@ -269,6 +302,22 @@ def api_account_approved_application():
     return generate_return_data(StatusCode.SUCCESS)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def api_game_room_join_game():
     data = flask.request.get_json()
 
@@ -290,7 +339,6 @@ def api_game_room_join_game():
             StatusCode.ERR_GAME_PLAYER_JOIN_GAME_FAILED,
             {'error_message': message})
     return generate_return_data(StatusCode.SUCCESS)
-
 
 def api_game_room_get_players():
 
