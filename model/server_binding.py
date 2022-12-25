c4e8a1d5631dc468ac9c15c3f102cbc8191a7bca
Yukiteru Li <wfly1998@sina.com>
2022-12-25 18:48:10 +0800

    model: update server binding with exception handler

---
+++ model/server_binding.py
@@ -0,0 +1,129 @@
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "/device:CPU:0"  # 指定 CPU
+import time
+import traceback
+from pathlib import Path
+
+from PIL import Image
+from tensorflow import keras
+
+from stable_diffusion_tf.stable_diffusion import StableDiffusion
+
+CWD = Path(os.getcwd())
+if os.path.basename(CWD) == 'model':
+    CWD = CWD.parent
+IMAGE_SAVE_PATH = CWD / 'data' / 'pic'
+
+if not IMAGE_SAVE_PATH.exists():
+    os.mkdir(IMAGE_SAVE_PATH)
+
+
+class Text2image:
+
+    def __init__(self,
+                 image_height: int = 64,
+                 image_width: int = 64,
+                 steps: int = 50,
+                 scale: float = 7.5,
+                 use_mixed_precision: bool = False) -> None:
+
+        self.image_height = image_height
+        self.image_width = image_width
+        self.steps = steps
+        self.scale = scale
+
+        if use_mixed_precision:
+            keras.mixed_precision.set_global_policy("mixed_float16")
+
+        self.init_stable_diffusion()
+
+    def init_stable_diffusion(self):
+        self.generator = StableDiffusion(img_height=self.image_height,
+                                         img_width=self.image_width,
+                                         jit_compile=False,
+                                         download_weights=False)
+
+    def __generate_image_with_func_params(self,
+                                          positive_list: list,
+                                          negative_list: list,
+                                          name: str,
+                                          rand_seed: int = -1):
+        filename = '{name}.png'.format(name=name)
+        path = IMAGE_SAVE_PATH / filename
+        if os.path.exists(path):
+            return False, 'File Exists!'
+
+        positive = None
+        negative = None
+
+        # positive prompt must exist
+        if not isinstance(positive_list, list):
+            return False, 'Invalid type for "positive_list"'
+        positive = ', '.join(positive_list)
+
+        # negative prompt can be empty
+        if isinstance(negative_list, list) and len(negative_list) > 0:
+            negative = ', '.join(negative)
+
+        # use current timestamp as random seed
+        if rand_seed < 0:
+            rand_seed = int(time.time())
+
+        image = self.generator.generate(
+            prompt=positive,
+            negative_prompt=negative,
+            num_steps=self.steps,
+            unconditional_guidance_scale=self.scale,
+            temperature=1,
+            batch_size=1,
+            seed=rand_seed,
+        )
+
+        Image.fromarray(image[0]).save(path)
+        return True, path
+
+    def generate_image_with_func_params(self,
+                                        positive_list: list,
+                                        negative_list: list,
+                                        name: str,
+                                        rand_seed: int = -1):
+        try:
+            return self.__generate_image_with_func_params(
+                positive_list, negative_list, name, rand_seed)
+        except Exception as e:
+            traceback.print_exc()
+            return False, 'Unknown problem'
+
+
+def main():
+    '''Just for test'''
+
+    param_capoo = [
+        'blue cat', 'single cat', 'fat', 'comic', 'six foots', 'full body',
+        'white background', 'black eyes', 'short legs', 'detail',
+        'high quality', 'master works'
+    ]
+    #param_def = ['serafuku', 'direct looking', 'eyeball', 'hair flower', 'close-up', 'gentle eyes', 'Hanfu pure wind', 'beauty', 'atmosphere light', 'shadow', 'detail', 'high quality', 'master works']
+    param = param_capoo
+    img_size = 256
+    steps = 50
+    scale = 7.5
+    mixed_precision = False
+    seed = int(time.time())
+    filename = 'size_{size}-step_{step}-scale_{scale}-{time}'.format(
+        size=img_size, step=steps, scale=scale, time=seed)
+
+    generator = Text2image(img_size,
+                           img_size,
+                           steps=steps,
+                           scale=scale,
+                           use_mixed_precision=mixed_precision)
+
+    ret = generator.generate_image_with_func_params(param,
+                                                    None,
+                                                    filename,
+                                                    rand_seed=seed)
+
+
+if __name__ == '__main__':
+    main()
--- model/text2image.py
+++ model/text2image.py
@@ -1,5 +1,6 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "/device:CPU:0" # 指定 CPU
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "/device:CPU:0"  # 指定 CPU
 # os.environ["CUDA_VISIBLE_DEVICES"] = "0" # 指定显卡
 
 # import tensorflow as tf
@@ -9,158 +10,96 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "/device:CPU:0" # 指定 CPU
 # sess = tf.compat.v1.Session(config=config)
 
 from tensorflow import keras
-from .stable_diffusion_tf.stable_diffusion import StableDiffusion
+from stable_diffusion_tf.stable_diffusion import StableDiffusion
 import argparse
 from PIL import Image
 from PIL.PngImagePlugin import PngInfo
 
-PLAYER_MAX_STORE_PIC_NUM = 1000
-IMAGE_SAVE_PATH = os.getcwd() + '/server/data/pic/'
+parser = argparse.ArgumentParser()
 
-class Text2image:
-    
-    def __init__(self, image_height:int=64, image_width:int=64, use_mixed_precision:bool=False) -> None:
+parser.add_argument(
+    "--prompt",
+    type=str,
+    nargs="?",
+    default=
+    "serafuku, direct looking, eyeball, hair flower, close-up, gentle eyes, Hanfu pure wind, beauty, atmosphere light, shadow, detail, high quality, master works",
+    help="the prompt to render",
+)
 
-        self.args = None
+parser.add_argument(
+    "--negative-prompt",
+    type=str,
+    help="the negative prompt to use (if any)",
+)
 
-        self.image_height = image_height
-        self.image_width = image_width
+parser.add_argument(
+    "--output",
+    type=str,
+    nargs="?",
+    default="output.png",
+    help="where to save the output image",
+)
 
-        if use_mixed_precision:
+parser.add_argument(
+    "--H",
+    type=int,
+    default=512,
+    help="image height, in pixels",
+)
 
-            print("Using mixed precision.")
-            keras.mixed_precision.set_global_policy("mixed_float16")
+parser.add_argument(
+    "--W",
+    type=int,
+    default=512,
+    help="image width, in pixels",
+)
 
-        self.__create_generator__()
+parser.add_argument(
+    "--scale",
+    type=float,
+    default=7.5,
+    help=
+    "unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+)
 
-    def init_console_args(self):
+parser.add_argument("--steps",
+                    type=int,
+                    default=50,
+                    help="number of ddim sampling steps")
 
-        parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--seed",
+    type=int,
+    help="optionally specify a seed integer for reproducible results",
+)
 
-        parser.add_argument(
-            "--prompt",
-            type=str,
-            nargs="?",
-            default="serafuku, direct looking, eyeball, hair flower, close-up, gentle eyes, Hanfu pure wind, beauty, atmosphere light, shadow, detail, high quality, master works",
-            help="the prompt to render",
-        )
+parser.add_argument(
+    "--mp",
+    default=False,
+    action="store_true",
+    help="Enable mixed precision (fp16 computation)",
+)
 
-        parser.add_argument(
-            "--negative-prompt",
-            type=str,
-            help="the negative prompt to use (if any)",
-        )
+args = parser.parse_args()
 
-        parser.add_argument(
-            "--output",
-            type=str,
-            nargs="?",
-            default="output.png",
-            help="where to save the output image",
-        )
+if args.mp:
+    print("Using mixed precision.")
+    keras.mixed_precision.set_global_policy("mixed_float16")
 
-        parser.add_argument(
-            "--H",
-            type=int,
-            default=512,
-            help="image height, in pixels",
-        )
-
-        parser.add_argument(
-            "--W",
-            type=int,
-            default=512,
-            help="image width, in pixels",
-        )
-
-        parser.add_argument(
-            "--scale",
-            type=float,
-            default=7.5,
-            help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
-        )
-
-        parser.add_argument(
-            "--steps", type=int, default=50, help="number of ddim sampling steps"
-        )
-
-        parser.add_argument(
-            "--seed",
-            type=int,
-            help="optionally specify a seed integer for reproducible results",
-        )
-
-        parser.add_argument(
-            "--mp",
-            default=False,
-            action="store_true",
-            help="Enable mixed precision (fp16 computation)",
-        )
-
-        self.args = parser.parse_args()
-
-        if self.args.mp:
-
-            print("Using mixed precision.")
-            keras.mixed_precision.set_global_policy("mixed_float16")
-
-    def __create_generator__(self):
-
-        if self.args:
-            self.generator = StableDiffusion(
-                img_height=self.args.H, 
-                img_width=self.args.W, 
-                jit_compile=False,
-                download_weights=False
-            )
-        else:
-            self.generator = StableDiffusion(
-                img_height=self.image_height, 
-                img_width=self.image_width, 
-                jit_compile=False,
-                download_weights=False
-            )
-
-    def generate_image_with_console_args(self):
-        
-        image = self.generator.generate(
-            self.args.prompt,
-            negative_prompt=self.args.negative_prompt,
-            num_steps=self.args.steps,
-            unconditional_guidance_scale=self.args.scale,
-            temperature=1,
-            batch_size=1,
-            seed=self.args.seed,
-        )
-
-        pnginfo = PngInfo()
-        pnginfo.add_text('prompt', self.args.prompt)
-        Image.fromarray(image[0]).save(self.args.output, pnginfo=pnginfo)
-        print(f"saved at {self.args.output}")
-
-    def generate_image_with_func_params(self, positive:str, negtive:str, name:str, rand_seed:int=0):
-
-        image = self.generator.generate(
-            prompt=positive,
-            negative_prompt=negtive,
-            num_steps=self.args.steps,
-            unconditional_guidance_scale=self.args.scale,
-            temperature=1,
-            batch_size=1,
-            seed=rand_seed,
-        )
-
-        for i in range(0, PLAYER_MAX_STORE_PIC_NUM):
-            path = IMAGE_SAVE_PATH + name + '/' + i + '.png'
-            if not os.path.exists(path):
-                Image.fromarray(image[0]).save(path)
-                return path
-
-        print('player could not store any more pic')
-        return None
-
-generator = Text2image(64, 64, True)
-
-def generate_image(positive:str, negtive:str, name:str, rand_seed:int=0):
-
-    generator.generate_image_with_func_params(positive, negtive, name, rand_seed)
+generator = StableDiffusion(img_height=args.H,
+                            img_width=args.W,
+                            jit_compile=False,
+                            download_weights=False)
+img = generator.generate(
+    args.prompt,
+    negative_prompt=args.negative_prompt,
+    num_steps=args.steps,
+    unconditional_guidance_scale=args.scale,
+    temperature=1,
+    batch_size=1,
+    seed=args.seed,
+)
+pnginfo = PngInfo()
+pnginfo.add_text('prompt', args.prompt)
+Image.fromarray(img[0]).save(args.output, pnginfo=pnginfo)
+print(f"saved at {args.output}")
